cc29e183f8d828bb5ba975b82c91b30bb1b82e07
e049773 <jorgeluis.fernandez.next@bbva.com>
2023-07-04 10:50:13 +0200

    arreglo en urlprefix del main

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (
 		return res, nil
 	}
 	// TODO: Mirar  si es twittergo o twitterGo
-	path := strings.Replace(request.PathParameters["twitterGo"], os.Getenv("UrlPrefix"), "", -1)
+	path := strings.Replace(request.PathParameters["twittergo"], os.Getenv("UrlPrefix"), "", -1)
 
 	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("path"), path)
 	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("method"), request.HTTPMethod)
