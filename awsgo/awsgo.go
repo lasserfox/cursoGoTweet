003e08a4b11fbc847a7a1a6c31efed3eda55f2c0
e049773 <jorgeluis.fernandez.next@bbva.com>
2023-07-04 11:53:55 +0200

    arreglo en la respuesta del secretmanager

--- awsgo/awsgo.go
+++ awsgo/awsgo.go
@@ -2,6 +2,7 @@ package awsgo
 
 import (
 	"context"
+	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 )
@@ -11,8 +12,11 @@ var Cfg aws.Config
 var err error
 
 func InicializandoAWS() {
+	fmt.Println("Iniciando AWS, creando context")
 	Ctx = context.TODO()
+	fmt.Println("Context creado, load Config")
 	Cfg, err = config.LoadDefaultConfig(Ctx, config.WithDefaultRegion("us-east-1"))
+	fmt.Println("Config : " + Cfg.Region)
 	if err != nil {
 		panic("Error al cargar la configuración de .aws/config " + err.Error())
 	}
