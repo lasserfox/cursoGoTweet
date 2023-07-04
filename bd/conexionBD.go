f17bf96ecc4202a5cb57e8b263b703f714efc233
e049773 <jorgeluis.fernandez.next@bbva.com>
2023-07-04 13:39:40 +0200

    arreglo en cadena de conexion de mongoDB

--- bd/conexionBD.go
+++ bd/conexionBD.go
@@ -16,8 +16,8 @@ func ConectarDB(ctx context.Context) error {
 	user := ctx.Value(models.Key("user")).(string)
 	password := ctx.Value(models.Key("password")).(string)
 	host := ctx.Value(models.Key("host")).(string)
-	fmt.Printf("Url Conexión: mongodb+srv//%s:*****@%s/?retryWrites=true&w=majority", user, host)
-	connStr := "mongodb+srv//" + user + ":" + password + "@" + host + "/?retryWrites=true&w=majority"
+	fmt.Printf("Url Conexión: mongodb+srv//%s:*****@%s/", user, host)
+	connStr := "mongodb+srv//" + user + ":" + password + "@" + host + "/"
 	fmt.Println("constr: " + connStr)
 
 	var clienOptions = options.Client().ApplyURI(connStr)
