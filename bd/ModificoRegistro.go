daa9c0e8822be4907fd5d7a147652b7c05aef150
e049773 <jorgeluis.fernandez.next@bbva.com>
2023-07-05 11:29:23 +0200

    agrego login

--- bd/ModificoRegistro.go
+++ bd/ModificoRegistro.go
@@ -3,6 +3,7 @@ package bd
 import (
 	"context"
 	"cursoGoTweet/models"
+	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -11,7 +12,7 @@ func ModificoRegistro(u models.Usuario, ID string) (bool, error) {
 	ctx := context.TODO()
 	db := MongoCN.Database(DatabaseName)
 	col := db.Collection("usuarios")
-
+	fmt.Println("Entro modifico Registro")
 	registro := make(map[string]interface{})
 
 	if len(u.Nombre) > 0 {
@@ -35,17 +36,27 @@ func ModificoRegistro(u models.Usuario, ID string) (bool, error) {
 		registro["ubicacion"] = u.Ubicacion
 	}
 	if len(u.SitioWeb) > 0 {
-		registro["sitioWeb"] = u.SitioWeb
+		registro["sitioweb"] = u.SitioWeb
 	}
-
+	fmt.Println("preparando updString ")
 	updtString := bson.M{
 		"$set": registro,
 	}
 
+	fmt.Println("preparando objID ")
 	objID, _ := primitive.ObjectIDFromHex(ID)
+	fmt.Println("preparando filtro ")
 	filtro := bson.M{"_id": bson.M{"$eq": objID}}
 
-	_, err := col.UpdateOne(ctx, filtro, updtString)
+	bsonData, err := bson.Marshal(filtro)
+	if err != nil {
+		fmt.Println("Error al convertir a BSON:", err)
+	}
+	strData := string(bsonData)
+	fmt.Println("filtro: " + strData)
+
+	_, err = col.UpdateOne(ctx, filtro, updtString)
+
 	if err != nil {
 		return false, err
 	}
